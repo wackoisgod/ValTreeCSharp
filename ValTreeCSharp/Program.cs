20048d5a8fe3f8fb3e382999d57ac18cd48fd612
Andrew Spiering <aspiering@gmail.com>
2016-06-18 15:16:19 -0700

    - Fixed up the ValTree class to be more in line with its C++ counterpart - Removed the concept of Siblings (no longer needed as they were not originally part of the C++ class) - Added support for "//" comments - Fixed logging to work correctly - The class is not Iterable and will return its childeren if iterated.

--- ValTreeCSharp/Program.cs
+++ ValTreeCSharp/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Xml.Schema;
+using TinyArms;
 
 namespace ValTreeCSharp
 {
@@ -9,6 +12,22 @@ namespace ValTreeCSharp
     {
         static void Main(string[] args)
         {
+            var testValTree = new ValTree();
+            string path = Directory.GetCurrentDirectory();
+            var worked = testValTree.Parse(path + "/TestData/PuzzleExample.txt");
+
+            var parent = testValTree.GetChild("properties");
+
+
+            testValTree.Log();
+
+            foreach (ValTree x in testValTree)
+            {
+                var y = x;
+                y.GetFirstChild();
+            }
+
+
         }
     }
 }
--- ValTreeCSharp/ValTree.cs
+++ ValTreeCSharp/ValTree.cs
@@ -2,289 +2,353 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Configuration;
+using System.Reflection;
+using System.Security.Policy;
 using System.Text;
 
-namespace ConsoleApplication2
+namespace TinyArms
 {
-	public class ValTree
-	{
-		static int findWhitespace(string s, int start)
-		{
-			int i = start;
-			for (; i < s.Length && !char.IsWhiteSpace(s[i]); i++)
-				;
-			return i;
-		}
+    public class ValTree : IEnumerable
+    {
+        private const int KMaxFileSize = 8*1024*1024;
+        private const string KCommentStartTag = "//";
+        private static ValTree blank = new ValTree();
 
-		static int findNonWhitespace(string s, int start)
-		{
-			int i = start;
-			for (; i < s.Length && char.IsWhiteSpace(s[i]) && s[i] != '\n' && s[i] != '\r'; i++)
-				;
-			return i;
-		}
+        static bool IsWhiteSpace(char c)
+        {
+            return (c == ' ' || c == '\t' || c == '\n' || c == '\r');
+        }
 
-		static int findNewline(string s, int start)
-		{
-			int i = start;
-			for (; i < s.Length && s[i] != '\n' && s[i] != '\r'; i++)
-				;
-			return i;
-		}
+        static int FindWhitespace(string s, int start)
+        {
+            int i = start;
+            for (; i < s.Length && !IsWhiteSpace(s[i]); i++)
+                ;
+            return i;
+        }
 
-		static int findAfterNewline(string s, int start)
-		{
-			int i = findNewline(s, start);
-			for (; i < s.Length && (s[i] == '\n' || s[i] == '\r'); i++)
-				;
-			return skipComment(s, i);
-		}
+        static int FindNonWhitespace(string s, int start)
+        {
+            int i = start;
+            for (; i < s.Length && IsWhiteSpace(s[i]) && s[i] != '\n' && s[i] != '\r'; i++)
+                ;
+            return i;
+        }
 
-		static int skipComment(string s, int start)
-		{
-			// so we have found a comment
-			if (start < s.Length && s[start] == ';')
-			{
-				return findAfterNewline(s, start);
-			}
+        static int FindNewline(string s, int start)
+        {
+            int i = start;
+            for (; i < s.Length && s[i] != '\n' && s[i] != '\r'; i++)
+                ;
+            return i;
+        }
 
-			return start;
-		}
+        static int FindAfterNewline(string s, int start)
+        {
+            int i = FindNewline(s, start);
+            for (; i < s.Length && (s[i] == '\n' || s[i] == '\r'); i++)
+                ;
 
-		private string _key;
-		private string _value;
-		private int _valInt;
-		private double _valFloat;
-		private List<ValTree> _children = new List<ValTree>();
-		private List<ValTree> _siblings = new List<ValTree>();
+            return i;
+        }
 
-		public List<ValTree> Children
-		{
-			get
-			{
-				return _children;
-			}
-		}
+        static int FindCommentOrNewline(string s, int start)
+        {
+            int i = start;
+            int nl = FindNewline(s, start);
+            int commentIndex = 0;
+            var commentTagSize = KCommentStartTag.Length;
 
-		public List<ValTree> Siblings
-		{
-			get
-			{
-				return _siblings;
-			}
-		}
+            while (i < nl)
+            {
+                var ss = s[i++];
+                var yy = KCommentStartTag[commentIndex++];
 
-		private void SetValInt()
-		{
-			int v = 0;
-			int.TryParse(_value, out v);
-			_valInt = v;
-		}
-		private void SetValFloat()
-		{
-			double v = 0;
-			double.TryParse(_value, out v);
-			_valFloat = v;
-		}
+                if (ss == yy)
+                {
+                    if (commentIndex == commentTagSize)
+                        return i - commentTagSize;
+                }
+                else
+                    commentIndex = 0;
+            }
 
-		private int GetDepth(string data, int pos)
-		{
-			for (int i = pos; i < data.Length; i++)
-				if (!char.IsWhiteSpace((data[i])))
-					return i - pos;
-			return -1;
-		}
+            return i;
+        }
 
-		private bool Parse(string data, ref int pos, bool firstSibling)
-		{
-			var depth = GetDepth(data, pos);
-			if (depth < 0) return false;
+        private string _key;
+        private string _value;
+        private int _valInt;
+        private double _valFloat;
+        private readonly List<ValTree> _children = new List<ValTree>();
+     
+        public List<ValTree> Children => _children;
 
-			int nextPos = skipComment(data, pos);
-			nextPos = findAfterNewline(data, nextPos);
-			int childDepth = GetDepth(data, nextPos);
+        private void SetValInt()
+        {
+            int v = 0;
+            int.TryParse(_value, out v);
+            _valInt = v;
+        }
 
-			int startPos = pos + depth;
-			pos = findWhitespace(data, pos + depth + 1);
+        private void SetValFloat()
+        {
+            double v = 0;
+            double.TryParse(_value, out v);
+            _valFloat = v;
+        }
 
+        private int GetDepth(string data, int pos)
+        {
+            for (int i = pos; i < data.Length; i++)
+                if (!IsWhiteSpace((data[i])))
+                    return i - pos;
+            return -1;
+        }
 
-			_key = data.Substring(startPos, pos < data.Length ? pos - startPos : data.Length);
+        private bool Parse(string data, ref int pos, int lastDepth)
+        {
+            int nextLineStart = FindAfterNewline(data, pos);
+            if (nextLineStart > KMaxFileSize || pos == nextLineStart)
+                return false;
 
-			if (_key.Length > 0)
-			{
-				pos = findNonWhitespace(data, pos);
-				int end = findNewline(data, pos);
-				if (pos < data.Length && end > pos)
-				{
-					_value = data.Substring(pos, end - pos);
+            if (FindCommentOrNewline(data, pos) <= FindNonWhitespace(data, pos))
+            {
+                pos = nextLineStart;
+                return Parse(data, ref pos, lastDepth);
+            }
 
-					SetValInt();
-					SetValFloat();
-				}
-			}
+            var depth = GetDepth(data, pos);
+            if (depth == lastDepth)
+            {
+                var dataSize = data.Length;
+                int startPos = pos + depth;
+                if (startPos < nextLineStart)
+                {
+                    pos = FindWhitespace(data, pos + depth + 1);
+                    _key = data.Substring(startPos, pos < dataSize ? pos - startPos : -1);
+                }
 
-			pos = nextPos;
+                if (_key.Length > 0)
+                {
+                    pos = FindNonWhitespace(data, pos);
+                    int end = FindCommentOrNewline(data, pos);
+                    if (pos < dataSize && end > pos)
+                    {
+                        _value = data.Substring(pos, end - pos);
+                        SetValInt();
+                        SetValFloat();
+                    }
+                }
 
-			if (childDepth > depth)
-			{ 
-				_children.Add(new ValTree());
-				if (!_children.Last().Parse(data, ref pos, true))
-					_children.RemoveAt(_children.Count - 1);
-					
-				childDepth = GetDepth(data, pos);
-			}
+                pos = nextLineStart;
+                depth = GetDepth(data, pos);
+            }
 
-			if (childDepth == depth && firstSibling)
-			{
-				bool success = true;
-				while (success && childDepth == depth)
-				{
-					_siblings.Add(new ValTree());
-					success = _siblings.Last().Parse(data, ref pos, false);
-					if (!success)
-						_siblings.RemoveAt(_siblings.Count - 1);
-						
-					childDepth = GetDepth(data, pos);
-				}
-				return _siblings.Count > 0;
-			}
+            // parse children
+            if (depth > lastDepth)
+            {
+                bool success = true;
+                lastDepth = depth;
+                do
+                {
+                    _children.Add(new ValTree());
+                    success = _children.Last().Parse(data, ref pos, depth);
+                    if (!success)
+                        _children.RemoveAt(_children.Count - 1);
 
-			return !IsNull();
-		}
+                    depth = GetDepth(data, pos);
+                } while (success && depth == lastDepth);
+            }
 
-		public ValTree()
-		{
-			_valInt = 0;
-			_valFloat = 0;
-		}
+            return !IsNull();
+        }
 
-		public ValTree(string inKey, string inValue)
-		{
-			Set(inKey, inValue);
-		}
+        public ValTree()
+        {
+            _valInt = 0;
+            _valFloat = 0;
 
-		public void Clear()
-		{
-			_key = String.Empty;
-			_value = String.Empty;
-			_valInt = 0;
-			_valFloat = 0.0;
-			_children.Clear();
-			_siblings.Clear();
+            Clear();
+        }
 
-		}
+        public ValTree(string inKey, string inValue)
+        {
+            Set(inKey, inValue);
+        }
 
-		public bool IsNull()
-		{
-			return (_key.Length <= 0 && _value.Length <= 0);
-		}
+        public void Clear()
+        {
+            _key = String.Empty;
+            _value = String.Empty;
+            _valInt = 0;
+            _valFloat = 0.0;
+            _children.Clear();
+        }
 
-		public string GetKey() { return _key; }
-		public string GetValue() { return _value; }
-		public int GetInt() { return _valInt; }
-		public double GetFloat() { return _valFloat; }
+        public bool IsNull()
+        {
+            return (_key.Length <= 0 && _value.Length <= 0);
+        }
 
-		public void Set(string key, string val)
-		{
-			_key = key;
-			_value = val;
-			SetValInt();
-			SetValFloat();
-		}
+        public string GetKey()
+        {
+            return _key;
+        }
 
-		public int Size() { return 0; }
+        public string GetValue()
+        {
+            return _value;
+        }
 
-		ValTree GetIndex(int index)
-		{
-			if (index > 0 && index - 1 < _siblings.Count())
-				return _siblings[index - 1];
+        public int GetInt()
+        {
+            return _valInt;
+        }
 
-			return this;
-		}
+        public double GetFloat()
+        {
+            return _valFloat;
+        }
 
-		public bool HasChildren()
-		{
-			return _children.Count > 0;
-		}
+        public void Set(string key, string val)
+        {
+            _key = key;
+            _value = val;
+            SetValInt();
+            SetValFloat();
+        }
 
-		public ValTree GetFirstChild()
-		{
-			return (HasChildren() ? _children.First() : null);
-		}
+        public int Size()
+        {
+            return _children.Count;
+        }
 
-		public ValTree GetChild(string key)
-		{
-			return _children.Where(x => x._key == _key).SingleOrDefault();
-		}
-		public ValTree GetSibling(string key)
-		{
-			if (key == _key)
-				return this;
+        public bool HasChildren()
+        {
+            return _children.Count > 0;
+        }
 
-			return _siblings.Where(x => x._key == key).SingleOrDefault();
-		}
+        public ValTree GetFirstChild()
+        {
+            return (HasChildren() ? _children.First() : null);
+        }
 
-		public void AddChild(ref ValTree v)
-		{
-			_children.Add(v);
-		}
-		public void AddSibling(ref ValTree v)
-		{
-			_siblings.Add(v);
-		}
+        public ValTree GetChild(string key)
+        {
+            return _children.SingleOrDefault(x => x._key == key);
+        }
 
-		public bool Parse(string filename)
-		{
-			Clear();
+        public ValTree GetIndex(int index)
+        {
+            return (index < _children.Count ? _children[index] : blank);
+        }
 
-			if (!System.IO.File.Exists(filename))
-				return false;
+        public void AddChild(ref ValTree v)
+        {
+            _children.Add(v);
+        }
 
-			var data = System.IO.File.ReadAllText(filename);
-			if (data.Length <= 0)
-				return false;
+        public ValTree Query(string query)
+        {
+            int x = query.IndexOf(".", StringComparison.Ordinal);
+            if (x == -1)
+            {
+                return GetChild(query);
+            }
 
-			int pos = 0;
-			Parse(data, ref pos, true);
+            string k = query.Substring(0, x);
+            string v = query.Substring(x + 1);
+            return k.Length > 0 ? GetChild(k).Query(v) : Query(v);
+        }
 
-			return true;
-		}
+        public void AddChild(string query, string value)
+        {
+            var pos = query.IndexOf(".", StringComparison.Ordinal);
+            if (pos == -1)
+            {
+                ValTree yy = new ValTree(query, value);
+                AddChild(ref yy);
+                return;
+            }
 
-		static void InternalLog(ref StringBuilder sb, ValTree inValue, int depth)
-		{
-			for (int i = 0; i < depth; i++)
-				sb.AppendFormat("\t");
+            string k = query.Substring(0, pos);
+            string v = query.Substring(pos + 1);
+            if (k.Length <= 0) return;
 
-			sb.AppendFormat(inValue.GetKey() + " " + inValue.GetValue());
-			if (inValue.HasChildren())
-			{
-				foreach (var c in inValue._children)
-				{
-					InternalLog(ref sb, c, depth + 1);
-				}
-			}
+            ValTree child = GetChild(k);
 
-			int y = 0;
-			foreach (var s in inValue._siblings)
-				if (y++ > 0)
-					InternalLog(ref sb, s, depth);
+            if (child == null)
+            {
+                ValTree xx = new ValTree(k, string.Empty);
+                xx.AddChild(v, value);
+                AddChild(ref xx);
+            }
+            else
+            {
+                child.AddChild(v, value);
+            }
+        }
 
-		}
+        public bool Parse(string filename)
+        {
+            Clear();
 
-		public bool Save(string filename)
-		{
-			var sb = new StringBuilder();
-			InternalLog(ref sb, this, 0);
-			System.IO.File.WriteAllText(filename, sb.ToString());
+            if (!System.IO.File.Exists(filename))
+                return false;
 
-			return true;
-		}
+            var data = System.IO.File.ReadAllText(filename);
+            if (data.Length <= 0)
+                return false;
 
-		public void Log()
-		{
-			var sb = new StringBuilder();
-			InternalLog(ref sb, this, 0);
-			Console.WriteLine(sb.ToString());
-		}
-	}
+            int pos = 0;
+            Parse(data, ref pos, -1);
+
+            return true;
+        }
+
+        static void InternalLog(ref StringBuilder sb, ValTree inValue, int depth)
+        {
+            if (inValue.GetKey().Length > 0 || inValue.GetValue().Length > 0)
+            {
+                for (int i = 0; i < depth; i++)
+                {
+                    sb.Append("\t");
+                }
+
+                sb.AppendLine(inValue.GetKey() + " " + inValue.GetValue());
+            }
+
+            for (int i = 0; i < inValue.Size(); i++)
+            {
+                var child = inValue.GetIndex(i);
+                InternalLog(ref sb, child, depth + 1);
+            }
+        }
+
+        public bool Save(string filename)
+        {
+            var sb = new StringBuilder();
+            InternalLog(ref sb, this, 0);
+            System.IO.File.WriteAllText(filename, sb.ToString());
+
+            return true;
+        }
+
+        public void Log()
+        {
+            var sb = new StringBuilder();
+            InternalLog(ref sb, this, 0);
+            Console.WriteLine(sb.ToString());
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (ValTree child in _children)
+            {
+                yield return child;
+            }
+        }
+    }
 }
